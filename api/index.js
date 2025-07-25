0303cab849a4a2df772a03a0ea7629c68940c709
harsh7493 <maurya2004h@gmail.com>
2025-07-25 09:18:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,18 +7,17 @@ const app = express();
 // Middleware
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000' // अपने फ्रंटएंड का URL यहाँ डालें
+  origin: 'http://localhost:3000' 
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// 'uploads' फ़ोल्डर को स्टैटिक फ़ोल्डर के रूप में सर्व करें
-// यह लाइन बहुत ज़रूरी है
+
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 // Routes
-app.use('/', require('./routes')); // आपकी मुख्य राउटर फ़ाइल
+app.use('/', require('./routes')); 
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
